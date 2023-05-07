01ea3a682dc45861fdc8ec70c3ebece4d6006370
Robbe Louwet <robbe.louwet@student.kuleuven.be>
2023-05-07 11:40:59 +0200

    seasons implemented

--- src/components/settings/Season.js
+++ src/components/settings/Season.js
@@ -1,22 +1,67 @@
 import { Slider } from '@mui/material';
 import { useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
 import Accordion from 'react-bootstrap/Accordion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSnowflake, faSun } from '@fortawesome/free-regular-svg-icons';
+import { faLeaf } from '@fortawesome/free-solid-svg-icons';
+import { faCanadianMapleLeaf } from '@fortawesome/free-brands-svg-icons';
 
 export default ({ eventKey }) => {
-    const [valuetext, setV] = useState("test")
+    function parseValue(value) {
+        return `${value}°C`;
+    }
+
     return <Accordion.Item eventKey={eventKey}>
-        <Accordion.Header>Season</Accordion.Header>
+        <Accordion.Header>
+
+        </Accordion.Header>
         <Accordion.Body>
-            <Slider
-                aria-label="Temperature"
-                defaultValue={25}
-                getAriaValueText={() => valuetext}
-                valueLabelDisplay="auto"
-                step={25}
-                marks
-                min={0}
-                max={100}
-            />
+            <Row style={{ "width": "100%", marginLeft: "10px", paddingRight: "10px" }}>
+                <Col className='p-0 pl-1' xs={12}><div className='d-block'>Season</div></Col>
+                <Col className='p-0' xs={12}>
+                    <Row class>
+                        <Col className='pr-0' xs={4}><FontAwesomeIcon size='2x' icon={faSnowflake} color='#57A5DD' /></Col>
+                        <Col className='p-0' xs={4}><FontAwesomeIcon size='2x' icon={faLeaf} color='#9EC044' /></Col>
+                        <Col className='p-0' xs={4}>
+                            <Row classN>
+                                <Col className='p-0' style={{ marginLeft: "-10px" }}><FontAwesomeIcon size='2x' icon={faSun} color='#F7DB4E' /></Col>
+                                <Col className='pl-0 text-end' style={{ marginRight: '10px' }}><FontAwesomeIcon size='2x' icon={faCanadianMapleLeaf} color="#D85B32" /></Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col xs={12}><Slider
+                    track={false}
+                    aria-label="Seasons"
+                    defaultValue={0}
+                    getAriaValueText={parseValue}
+                    valueLabelDisplay="off"
+                    //step={10} //  (waarvan middelste 4 de values)
+                    marks={labels}
+                    min={0}
+                    max={3}
+                /></Col>
+            </Row>
         </Accordion.Body>
     </Accordion.Item>
 }
+
+const labels = [
+    {
+        value: 0,
+        label: "Winter"
+    },
+    {
+        value: 1,
+        label: "Spring"
+    },
+    {
+        value: 2,
+        label: "Summer",
+    },
+    {
+        value: 3,
+        label: "Autumn",
+    }
+]
