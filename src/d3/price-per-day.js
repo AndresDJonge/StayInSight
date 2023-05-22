860f790af07d2e83c670b2318bb2761a08eb8325
Thomas Busaan <thomas.busaan@student.kuleuven.be>
2023-05-22 18:54:10 +0200

    Beginsels opmaak

--- src/d3/price-per-day.js
+++ src/d3/price-per-day.js
@@ -66,9 +66,8 @@ export default (data, selectedRange) => {
         .attr("y", (bin) => y(bin.count))
         .attr("width", _ => x(1) - x(0))
         .attr("height", (bin) => height - y(bin.count))
-        .attr("fill", "#69b3a2")
-        .attr("fill-opacity", bin => {
-            return !inRange(bin.start, selectedRange) ? 0.2 : 1
+        .attr("fill", bin => {
+            return !inRange(bin.start, selectedRange) ? colorScale("color-complement-1") : colorScale("color-complement-4")
         });
 
 
@@ -112,7 +111,21 @@ export function updateChart(data, selectedRange) {
     const bins = getBins(prices);
     const bars = svg.selectAll('rect').data(bins)
 
-    bars.attr("fill-opacity", bin => {
-        return !inRange(bin.start, selectedRange) ? 0.2 : 1
+    bars.attr("fill", bin => {
+        return !inRange(bin.start, selectedRange) ? colorScale("color-complement-1") : colorScale("color-complement-4")
     });
 }
+
+var colorScale = d3.scaleOrdinal()
+.domain([
+  "color-primary-0", "color-primary-1", "color-primary-2", "color-primary-3", "color-primary-4",
+  "color-secondary-1-0", "color-secondary-1-1", "color-secondary-1-2", "color-secondary-1-3", "color-secondary-1-4",
+  "color-secondary-2-0", "color-secondary-2-1", "color-secondary-2-2", "color-secondary-2-3", "color-secondary-2-4",
+  "color-complement-0", "color-complement-1", "color-complement-2", "color-complement-3", "color-complement-4"
+])
+.range([
+  "#42A4F5", "#98CDF9", "#69B7F7", "#1A8FF0", "#0780E4",
+  "#5552F7", "#A3A1FA", "#7976F8", "#302CF2", "#140FE8",
+  "#FFCE37", "#FFE594", "#FFD962", "#FFC30B", "#FFC000",
+  "#FFAB37", "#FFD294", "#FFBD62", "#FF980B", "#FF9400"
+]);
