d6f9c89847d5927418db5d3be17d66b4901a9859
Andres De Jonge <andresdejonge@rocketmail.com>
2023-05-22 16:57:08 +0200

    cherrypicking #2

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -6,12 +6,17 @@ export default ({ filteredData }) => {
     const [season, setSeaon] = useState("Winter")
 
     useEffect(() => {
-        //console.log("Summary based on filteredData: ", filteredData)
         const price = filteredData.map(e => e.avg_price).reduce((a, cv) => a + cv, 0) / filteredData.length
 
         setSeaon(filteredData[0]["season"])
         setPrice(price ? price : 0)
     }, [])
 
-    return <div className="lead">{price ? <>You should ask €{Math.round(price, 2)} during {season}</> : <>Select your parameters</>}</div>
+
+    return <div className="lead">
+        {price ?
+            `You should ask €${Math.round(price, 2)} during ${season}` :
+            'Select your parameters'
+        }
+    </div>
 }
